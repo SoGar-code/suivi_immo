41ac8e56f24b30d3e2b25ecf29efbb473e05e77e
SoGar-code <olivier.gabriel@se.com>
2024-08-02 22:15:15 +0200

    Feat: stats logement social + ajout scraping

--- explo_scraping.py
+++ explo_scraping.py
@@ -5,10 +5,11 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import datetime as dt
+from pathlib import Path
 
 from pydantic import BaseModel
 from typing import List, Tuple, Optional
-from sqlalchemy import create_engine, text
+#from sqlalchemy import create_engine, text
 
 from bnp_re_parser import parser as bnp_parser
 from jll_parser import full_parser as jll_parser
@@ -31,7 +32,29 @@ def save_data(df):
     """
     Save provided dataframe to database
     """
-    df.to_sql(name=BUREAUX_TABLE, con=get_engine(), if_exists="append", index=False)
+    #df.to_sql(name=BUREAUX_TABLE, con=get_engine(), if_exists="append", index=False)
+    today_str = dt.date.today().strftime("%Y-%M-%d")
+    save_path = Path(".")/ f"data/bureaux_{today_str}.xlsx"
+    print("Saving to: ", str(save_path))
+    df.to_excel(save_path)
+
+
+def _process_item(source_str, parser) -> pd.DataFrame:
+    """
+    Apply the provided parser and return the output as dataframe
+    """
+    nbr_items, list_items = parser()
+
+    if nbr_items != len(list_items):
+        print(f"Something wrong for {source_str}! (multiple pages?)")
+        print(f"{nbr_items=} and {len(list_items)=}")
+    else:
+        print(f"Fetched {nbr_items} items from {source_str}.")
+
+    aux_df = pd.DataFrame.from_records([item.__dict__ for item in list_items])
+    aux_df["date"] = dt.date.today()
+    aux_df["source"] = source_str
+    return aux_df
     
 
 def scrape_save():
@@ -42,25 +65,10 @@ def scrape_save():
         "JLL": jll_parser,
         "BNP": bnp_parser
     }
-    today_dt = dt.date.today()
-    df = pd.DataFrame()
-    
-    for source_str, parser in parser_dict.items():
-        nbr_items, list_items = parser()
-
-        if nbr_items != len(list_items):
-            print(f"Something wrong for {source_str}! (multiple pages?)")
-            print(f"{nbr_items=} and {len(list_items)=}")
-        else:
-            print(f"Fetched {nbr_items} items from {source_str}.")
-
-        aux_df = pd.DataFrame.from_records([item.__dict__ for item in list_items])
-        aux_df["date"] = today_dt
-        aux_df["source"] = source_str
-        
-        df=df.append(aux_df)
+    df = pd.concat([_process_item(source_str, parser) for source_str, parser in parser_dict.items()], ignore_index=True)
     
     save_data(df)
+    return df
 
     
 def load_data() -> pd.DataFrame:
